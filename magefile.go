3256e1ec4a0ec6803b0b83029ecfeb2efe86c32e
Alex Kristiansen <alex.kristiansen@elastic.co>
2022-03-30 10:49:25 -0700

    format, cleanup

--- magefile.go
+++ magefile.go
@@ -1,6 +1,19 @@
-// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-// or more contributor license agreements. Licensed under the Elastic License;
-// you may not use this file except in compliance with the Elastic License.
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 //go:build mage
 // +build mage
@@ -15,7 +28,7 @@ import (
 )
 
 func GenProto() {
-	sh.Run("protoc", "-I", " shipper", "-I", "shipper/vendor", "--go_out=./shipper", "--go-grpc_out=./shipper", "--go_opt=paths=source_relative", "--go-grpc_opt=paths=source_relative", "shipper/shipper.proto")
+	sh.Run("protoc", "-Ishipper", "-Ishipper/vendor", "--go_out=./shipper", "--go-grpc_out=./shipper", "--go_opt=paths=source_relative", "--go-grpc_opt=paths=source_relative", "shipper/shipper.proto")
 }
 
 func Build() {
