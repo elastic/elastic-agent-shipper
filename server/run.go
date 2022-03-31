a0f5c1bb314db42de90e07c755acd92e38103d0e
Alex K <8418476+fearful-symmetry@users.noreply.github.com>
2022-03-31 11:57:31 -0700

    Create initial Skeleton of the shipper and gRPC service (#18)
    * init commit
    
    * format, cleanup
    
    * rename protobuf dir, add lint
    
    * make linter mostly happy
    
    * move to auto-generated linter file
    
    * more linter cleanup, see if we can get ci working
    
    * add file skips to ci
    
    * update magefile

---
+++ server/run.go
@@ -0,0 +1,72 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package server
+
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/elastic/elastic-agent-libs/logp"
+	"github.com/elastic/elastic-agent-shipper/config"
+
+	pb "github.com/elastic/elastic-agent-shipper/api"
+)
+
+// LoadAndRun loads the config object and runs the gRPC server
+func LoadAndRun() error {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		return fmt.Errorf("error reading config: %w", err)
+
+	}
+
+	err = logp.Configure(cfg.Log)
+	if err != nil {
+		return fmt.Errorf("failed to initialize logger: %w", err)
+
+	}
+
+	return Run(cfg)
+}
+
+// Run starts the gRPC server
+func Run(cfg config.ShipperConfig) error {
+	log := logp.L()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", cfg.Port))
+	if err != nil {
+		return fmt.Errorf("failed to listen: %w", err)
+	}
+
+	var opts []grpc.ServerOption
+	if cfg.TLS {
+		creds, err := credentials.NewServerTLSFromFile(cfg.Cert, cfg.Key)
+		if err != nil {
+			return fmt.Errorf("failed to generate credentials %w", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
+	r := shipperServer{logger: log}
+	pb.RegisterProducerServer(grpcServer, r)
+	log.Debugf("gRPC server is listening on port %d", cfg.Port)
+	return grpcServer.Serve(lis)
+
+}
