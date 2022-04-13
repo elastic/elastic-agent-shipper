ac4ab91b47dabb0d4dbd9f76953e0a96c2dc9d3b
Alex Kristiansen <alex.kristiansen@elastic.co>
2022-04-13 10:20:24 -0700

    get formatter and mage working

--- magefile.go
+++ magefile.go
@@ -1,19 +1,6 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
 
 //go:build mage
 // +build mage
@@ -29,7 +16,14 @@ import (
 	_ "github.com/elastic/elastic-agent/dev-tools/mage/target/common"
 	// mage:import
 	"github.com/elastic/elastic-agent-libs/dev-tools/mage"
+
+	devtools "github.com/elastic/elastic-agent/dev-tools/mage"
 )
+
+func init() {
+	devtools.BeatLicense = "Elastic License"
+	devtools.BeatDescription = "Shipper processes, queues, and ships data."
+}
 
 // Aliases are shortcuts to long target names.
 // nolint: deadcode // it's used by `mage`.
