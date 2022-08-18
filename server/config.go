5df6b7835bc3cdd895c2aa8e064a289c1639aa97
Craig MacKenzie <craig.mackenzie@elastic.co>
2022-08-18 10:10:52 -0400

    Add a detailed README and clean up the configuration file (#94)
    * Add a more complete project readme.
    * Include design goals and architecture details.
    * Include a section on client development.
    * Include contributing and development sections.
    * Add a more reference like config.
    * Document all fields in the config file.
    * Move some general parameters under the server object.
    * Add issue and PR templates.
    * Clarify where queue defaults came from.
    * Clarify whether events are delayed or discarded.
    
    Co-authored-by: Denis Rechkunov <denis@rdner.de>

--- server/config.go
+++ server/config.go
@@ -4,6 +4,10 @@
 
 package server
 
+const (
+	defaultPort = 50051
+)
+
 type Config struct {
 	// StrictMode means that every incoming event will be validated against the
 	// list of required fields. This introduces some additional overhead but can
@@ -11,11 +15,21 @@ type Config struct {
 	// Normally, it should be disabled during production use and enabled for testing.
 	// In production it is preferable to send events to the output if at all possible.
 	StrictMode bool `config:"strict_mode"`
+	// Whether to use TLS for the gRPC connection.
+	TLS bool `config:"tls"`
+	// TLS cert file, if TLS is enabled
+	Cert string `config:"cert"`
+	// TLS Keyfile, if specified
+	Key string `config:"key"`
+	// Port to listen on
+	Port int `config:"port"`
 }
 
 // DefaultConfig returns default configuration for the gRPC server
 func DefaultConfig() Config {
 	return Config{
 		StrictMode: false,
+		TLS:        false,
+		Port:       defaultPort,
 	}
 }
