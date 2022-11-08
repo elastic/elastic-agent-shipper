444261e29a5a0d4b6b27e69da89c0f672ed60e70
Michal Pristas <michal.pristas@gmail.com>
2022-11-08 09:36:32 +0100

    add versioning information

---
+++ cmd/version.go
@@ -0,0 +1,26 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/elastic/elastic-agent-shipper/tools"
+	"github.com/spf13/cobra"
+)
+
+func newVersionCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Version returns version and build information.",
+		Long:  `Version returns version and build information.`,
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Printf("elastic-agent-shipper\tversion: %s\n", tools.GetDefaultVersion())
+			fmt.Printf("\tbuild_commit: %s\tbuild_time: %s\tsnapshot_build: %v\n", tools.Commit(), tools.BuildTime(), tools.Snapshot())
+		},
+	}
+
+	return cmd
+}
